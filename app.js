6e63e881be7eb9089aeae4052a0a94ba83d717e3
Pascale Hendricks <pascalehendricks@yahoo.com>
2022-03-24 12:20:42 -0700

    fixed error

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+let answer1 = prompt("Do you like trees?");
+let answer2 = prompt("What is your favorite tree?");
+
+    if ( answer2 === "yes") {
+document.write("<li>That's a nice choice, here are some others too</li>")
+}
+
+
+    else  {
+        document.write("<li>I suggest liking trees </li>");  }
