c7125efc6e692f5b2076d453c781099d0b3853c9
Pascale Hendricks <pascalehendricks@yahoo.com>
2022-03-24 12:29:48 -0700

    fixing question

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 let answer1 = prompt("Do you like trees?");
 let answer2 = prompt("What is your favorite tree?");
 
-    if ( answer2 === "yes") {
+    if ( answer1 === "yes") {
 document.write("<li> That's a nice choice, here are some others too </li>")
 }
 
